4920709c8edfde9903f14cfabcb38de6ac4eb59b
Dmitriy Andrushko <dmytro.andrushko@gmail.com>
2018-09-12 14:31:07 +0300

    squash! Status checking in plugin logic modified

--- ec2-instance/plugin.py
+++ ec2-instance/plugin.py
@@ -12,9 +12,10 @@ def start(*args, **_):
     conn = AWSClinet(service_name, aws_props['region_name'],
                        aws_props['aws_access_key_id'], aws_props['aws_secret_access_key'])
 
-#    if ctx.operation.retry_number == 0:
-#        conn.client.start_instances(InstanceIds=[instance_id])
-    #Diving deep into the response dict to get instance state object
+    if ctx.operation.retry_number == 0:
+        conn.client.start_instances(InstanceIds=[instance_id])
+
+    # Diving deep into the response dict to get instance state object
     state = conn.client.describe_instances(InstanceIds=[instance_id])['Reservations'][0]['Instances'][0]['State']
     if state['Name'] != 'active':
         return ctx.operation.retry(message='Waiting for the instance to become Active',
@@ -33,6 +34,7 @@ def stop(*args, **_):
     if ctx.operation.retry_number == 0:
          conn.client.stop_instances(InstanceIds=[instance_id])
 
+    # Diving deep into the response dict to get instance state object
     state = conn.client.describe_instances(InstanceIds=[instance_id])['Reservations'][0]['Instances'][0]['State']
     if state['Name'] != 'stopped':
         return ctx.operation.retry(message='Waiting for the instance to be stopped',
